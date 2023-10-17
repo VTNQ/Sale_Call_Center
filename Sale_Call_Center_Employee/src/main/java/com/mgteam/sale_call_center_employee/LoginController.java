dd13179c3818b51ce2d06ecdae1ac517192e25ca
Vo Tan Nam Quoc <115608795+VTNQ@users.noreply.github.com>
2023-10-17 10:53:06 +0700

    update employee

--- Sale_Call_Center_Employee/src/main/java/com/mgteam/sale_call_center_employee/LoginController.java
+++ Sale_Call_Center_Employee/src/main/java/com/mgteam/sale_call_center_employee/LoginController.java
@@ -1,9 +1,12 @@
 package com.mgteam.sale_call_center_employee;
 
+import com.mgteam.sale_call_center_employee.dialog.DialogAlert;
 import com.mgteam.sale_call_center_employee.util.DBConnection;
 import com.mgteam.sale_call_center_employee.util.MD5;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoIterable;
 import com.mongodb.client.model.Filters;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -20,10 +23,38 @@ public class LoginController {
 
     @FXML
     void login(ActionEvent event) {
-        if(!UsernameField.getText().isEmpty()&&!PasswordField.getText().isEmpty()){
+        if(UsernameField.getText().isEmpty()&&PasswordField.getText().isEmpty()){
+            UsernameField.getStyleClass().add("text_field_error");
+            UsernameField.applyCss();
+            PasswordField.getStyleClass().add("text_field_error");
+            PasswordField.applyCss();
+            DialogAlert.DialogError("Username and Password is require");
+        }else if(UsernameField.getText().isEmpty()){
+            UsernameField.getStyleClass().add("text_field_error");
+            UsernameField.applyCss();
+            DialogAlert.DialogError("Username is require");
+        }else if(PasswordField.getText().isEmpty()){
+            PasswordField.getStyleClass().add("text_field_error");
+            PasswordField.applyCss();
+            DialogAlert.DialogError("Password is require");
+        }else if(!UsernameField.getText().isEmpty()&&!PasswordField.getText().isEmpty()){
             MongoCollection<Document>empCollection =DBConnection.getConnection().getCollection("Employee");
             Bson filter = Filters.and(Filters.eq("Username", MD5.Md5(UsernameField.getText())),Filters.eq("Password",MD5.Md5(PasswordField.getText())));
-            empCollection.find(filter);
+            MongoIterable<Document> results=empCollection.find(filter);
+            boolean isFound=false;
+            for (Document result : results) {
+                isFound=true;
+            }
+            if(isFound==true){
+                try {
+                    DialogAlert.DialogSuccess("Login Success");
+                    App.setRoot("Main");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }else{
+                DialogAlert.DialogError("Account not exist");
+            }
         }
     }
 }
