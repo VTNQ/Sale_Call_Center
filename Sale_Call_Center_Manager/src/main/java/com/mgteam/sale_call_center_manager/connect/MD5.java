21fb11552f5d9d2ab9580988c4b5788add5fbe93
tranp6648 <tranp6648@gmail.com>
2023-10-19 06:52:52 +0700

    update Manager

---
+++ Sale_Call_Center_Manager/src/main/java/com/mgteam/sale_call_center_manager/connect/MD5.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mgteam.sale_call_center_manager.connect;
+
+import java.security.MessageDigest;
+import java.util.Base64;
+
+/**
+ *
+ * @author tranp
+ */
+public class MD5 {
+     public static String encryPassword(String input) {
+        try {
+            String base64Encode = Base64.getEncoder().encodeToString(input.getBytes());
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] md5Byte = md.digest(base64Encode.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : md5Byte) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- Sale_Call_Center_Manager/src/main/java/module-info.java
+++ Sale_Call_Center_Manager/src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com.mgteam.sale_call_center_manager {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.mongodb.bson;
+    requires java.mail;
+    requires org.mongodb.driver.core;
+    requires org.mongodb.driver.sync.client;
     opens com.mgteam.sale_call_center_manager to javafx.fxml;
     exports com.mgteam.sale_call_center_manager;
 }
