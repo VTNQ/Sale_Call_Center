8c42e250d67d27dd21c49399f66b1495952a58b8
tranp6648 <tranp6648@gmail.com>
2023-11-12 17:25:10 +0700

    update Manager

--- Sale_Call_Center_Manager/src/main/java/com/mgteam/sale_call_center_manager/LoginController.java
+++ Sale_Call_Center_Manager/src/main/java/com/mgteam/sale_call_center_manager/LoginController.java
@@ -121,12 +121,13 @@ public class LoginController implements Initializable {
                 VBox vbox = new VBox(progressBar);
                 vbox.setSpacing(10);
                 vbox.setAlignment(Pos.CENTER);
-
+                
                 Scene scene = new Scene(vbox, 200, 60);
                 
                 Stage progressDialogStage = new Stage();
                  progressDialogStage.setResizable(false);
                 progressDialogStage.setScene(scene);
+                
 
                 Task<Void> loginTask = new Task<Void>() {
                     @Override
